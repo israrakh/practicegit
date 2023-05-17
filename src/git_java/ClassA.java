0186bbed385b7d057eab71f51307c7532e50abeb
Israr <israr.akh03@gmail.com>
2023-05-16 21:44:13 -0400

    Adding My New Repository

---
+++ src/git_java/ClassA.java
@@ -0,0 +1,8 @@
+package git_java;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("Class A");
+	}
+}
---
+++ src/git_java/ClassB.java
@@ -0,0 +1,8 @@
+package git_java;
+
+public class ClassB {
+	
+	public static void main(String[] args) {
+		System.out.println("Class B");
+	}
+}
